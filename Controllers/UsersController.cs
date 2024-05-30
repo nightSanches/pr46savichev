1ae7e73e85b98b588b78affbb5a8cc5a23bb6e09
nightSanches <galesavich@gmail.com>
2024-05-30 21:29:35 +0500

    Разработанный контроллер авторизации пользователя. Савичев

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using pr45savichev.Context;
+using pr45savichev.Model;
+using System;
+using System.Linq;
+
+namespace pr45savichev.Controllers
+{
+    [Route("api/UsersController")]
+    [ApiExplorerSettings(GroupName = "v2")]
+    public class UsersController : Controller
+    {
+        /// <summary>
+        /// Авторизация пользователя
+        /// </summary>
+        /// <param name="Login"></param>
+        /// <param name="Password"></param>
+        /// <returns>Данный метод преднозначен для авторизации пользователя на сайте</returns>
+        /// <response code="200">Пользователь успешно авторизован</response>
+        /// <response code="403">Ошибка запроса, данные не указаны</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("SingIn")]
+        [HttpPost]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(500)]
+        public ActionResult SingIn([FromForm] string Login, [FromForm] string Password)
+        {
+            if (Login == null || Password == null) return StatusCode(403);
+            try
+            {
+                Users User = new UsersContext().Users.Where(x => x.Login == Login && x.Password == Password).First();
+                return Json(User);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
