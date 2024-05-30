9f1a266cd89bf7296eb6e39aea5aa0f20ac1f9d1
nightSanches <galesavich@gmail.com>
2024-05-30 22:10:43 +0500

    Разработанный метод регистрации пользователя. Савичев

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -13,8 +13,8 @@ namespace pr45savichev.Controllers
         /// <summary>
         /// Авторизация пользователя
         /// </summary>
-        /// <param name="Login"></param>
-        /// <param name="Password"></param>
+        /// <param name="Login">Логин</param>
+        /// <param name="Password">Пароль</param>
         /// <returns>Данный метод преднозначен для авторизации пользователя на сайте</returns>
         /// <response code="200">Пользователь успешно авторизован</response>
         /// <response code="403">Ошибка запроса, данные не указаны</response>
@@ -32,7 +32,42 @@ namespace pr45savichev.Controllers
                 Users User = new UsersContext().Users.Where(x => x.Login == Login && x.Password == Password).First();
                 return Json(User);
             }
-            catch (Exception ex)
+            catch (Exception exp)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        /// <summary>
+        /// Регистрация нового пользователя
+        /// </summary>
+        /// <param name="Login">Логин</param>
+        /// <param name="Password">Пароль</param>
+        /// <returns>Данный метод предназначен для регистрации пользователя</returns>
+        /// /// <response code="200">Пользователь успешно зарегистрирован</response>
+        /// /// <response code="403">Ошибка запроса, данные не указаны</response>
+        /// /// <response code="500">При выполнении запроса возникла ошибка</response>
+        [Route("RegIn")]
+        [HttpPost]
+        [ProducesResponseType(typeof(Users), 200)]
+        [ProducesResponseType(403)]
+        [ProducesResponseType(500)]
+        public ActionResult RegIn([FromForm] string Login, [FromForm] string Password)
+        {
+            if (Login == null || Password == null) return StatusCode(403);
+            try
+            {
+                var newUser = new UsersContext();
+                Users User = new Users()
+                {
+                    Login = Login,
+                    Password = Password
+                };
+                newUser.Users.Add(User);
+                newUser.SaveChanges();
+                return Json(newUser);
+            }
+            catch (Exception exp)
             {
                 return StatusCode(500);
             }
